816eaeca6760ce68d2bebfeadbaa5a8da30e14a4
pekpuglia <pepuglia@gmail.com>
2022-08-04 14:26:24 -0300

    cea interface

--- nozzle_design/src/cea_interface.jl
+++ nozzle_design/src/cea_interface.jl
@@ -1,4 +1,98 @@
+"Interface com o RocketCEA para tubeiras de gás frio"
 module CEAInterface
-    using PyCall
-    const CEA_OBJ = pyimport("rocketcea.cea_obj")
+using PyCall
+const CEA_OBJ = pyimport("rocketcea.cea_obj")
+
+#câmara infinita
+struct NozzleConditions
+    Pc::Float64
+    Pamb::Float64
+    propellant::String
+    cea_obj::PyObject
+    function NozzleConditions(Pc::Real,
+                Pamb::Real,
+                propellant::String
+                )
+        new(Pc, Pamb, propellant, 
+            CEA_OBJ.CEA_Obj(propName=propellant))
+    end
+end
+
+#(Isp, condição::String)
+function get_ambient_Isp(nozzle_cond::NozzleConditions,
+                                 exp_ratio::Float64)
+    #considera λ?
+    return nozzle_cond.cea_obj.estimate_Ambient_Isp(
+        Pc=nozzle_cond.Pc, 
+        eps=exp_ratio, 
+        Pamb=nozzle_cond.Pamb)
+end
+
+# CFcea,CFfrozen, mode
+function get_ambient_Cf(nozzle_cond::NozzleConditions,
+                                exp_ratio::Float64)
+    return nozzle_cond.cea_obj.getFrozen_PambCf(
+        Pamb=nozzle_cond.Pamb,
+        Pc=nozzle_cond.Pc,
+        eps=exp_ratio
+    )
+end
+
+function get_Cstar(nozzle_cond::NozzleConditions)
+    return nozzle_cond.cea_obj.get_Cstar(
+        Pc=nozzle_cond.Pc)
+end
+
+#vector chamber, throat, exit
+function get_densities(nozzle_cond::NozzleConditions,
+    exp_ratio::Float64)
+    return nozzle_cond.cea_obj.get_Densities(
+        Pc=nozzle_cond.Pc,
+        eps=exp_ratio
+    )
+end
+
+#errado? M2 deveria depender de Pamb
+function get_exit_mach(nozzle_cond::NozzleConditions,
+    exp_ratio::Float64)
+    return nozzle_cond.cea_obj.get_MachNumber(
+        Pc=nozzle_cond.Pc,
+        eps=exp_ratio
+    )
+end
+
+function get_sonic_speeds(nozzle_cond::NozzleConditions,
+    exp_ratio::Float64)
+    return nozzle_cond.cea_obj.get_SonicVelocities(
+        Pc=nozzle_cond.Pc,
+        eps=exp_ratio
+    )
+end
+
+function get_temperatures(nozzle_cond::NozzleConditions,
+    exp_ratio::Float64)
+    return nozzle_cond.cea_obj.get_Temperatures(
+        Pc=nozzle_cond.Pc,
+        eps=exp_ratio
+    )
+end
+
+function get_exp_ratio(nozzle_cond::NozzleConditions)
+    return nozzle_cond.cea_obj.get_eps_at_PcOvPe(
+        Pc=nozzle_cond.Pc,
+        PcOvPe=nozzle_cond.Pc/nozzle_cond.Pamb
+    )
+end
+
+function add_new_propellant(name::String,
+    elements::Vector{Char}, amounts::Vector{Int},
+    enthalpy_cal::Float64, temperature_K::Float64,
+    rho_grams_cm3::Float64)
+    card = "
+    name $name " * prod("$el $am " for (el, am) in zip(elements, amounts)) * "wt%=100.00
+    h,cal=$enthalpy_cal     t(k)=$temperature_K  rho.g/cc=$rho_grams_cm3
+    "
+    CEA_OBJ.add_new_propellant(name, card)
+end
+
 end
--- nozzle_design/test/test.jl
+++ nozzle_design/test/test.jl
@@ -1,10 +1,80 @@
-using Test
 module RocketCeaTest
-using PyCall
+using Test
+include("../src/cea_interface.jl")
+#https://rocketcea.readthedocs.io/en/latest/quickstart.html
 function test_rocket_cea()
-    cea_obj = pyimport("rocketcea.cea_obj")
-    C = cea_obj.CEA_Obj(oxName="LOX", fuelName="LH2")
-    return C.get_Isp()
+    C = CEAInterface.CEA_OBJ.CEA_Obj(oxName="LOX", fuelName="LH2")
+    return C.get_Isp() ≈ 374.30361765576265
+end
+
+function test_get_ambient_Isp()
+    #monoprop padrao
+    nozzle_cond = CEAInterface.NozzleConditions(5.0, 1.0, "HYD40")
+    return CEAInterface.get_ambient_Isp(nozzle_cond, 1.5) isa Tuple
+end
+
+function test_get_ambient_cf()
+    nozzle_cond = CEAInterface.NozzleConditions(5.0, 1.0, "HYD40")
+    return CEAInterface.get_ambient_Cf(nozzle_cond, 1.5) isa Tuple
+end
+
+function test_get_Cstar()
+    nozzle_cond = CEAInterface.NozzleConditions(5.0, 1.0, "HYD40")
+    return CEAInterface.get_Cstar(nozzle_cond) isa Float64
+end
+
+function test_get_densities()
+    nozzle_cond = CEAInterface.NozzleConditions(5.0, 1.0, "HYD40")
+    dens = CEAInterface.get_densities(nozzle_cond, 1.5)
+    return all(dens[2:end] .< dens[1:(end-1)])    
+end
+
+function test_get_exit_mach()
+    nozzle_cond = CEAInterface.NozzleConditions(5.0, 1.0, "HYD40")
+    return CEAInterface.get_exit_mach(nozzle_cond, 1.5) isa Float64
+end
+
+function test_get_sonic_speeds()
+    nozzle_cond = CEAInterface.NozzleConditions(5.0, 1.0, "HYD40")
+    speeds = CEAInterface.get_sonic_speeds(nozzle_cond, 1.5)
+    return all(speeds[2:end] .< speeds[1:(end-1)])
+end
+
+function test_get_temperatures()
+    nozzle_cond = CEAInterface.NozzleConditions(5.0, 1.0, "HYD40")
+    temps = CEAInterface.get_temperatures(
+    nozzle_cond, 1.5)
+    return all(temps[2:end] .< temps[1:(end-1)])
+end
+
+function test_get_exp_ratio()
+    nozzle_cond = CEAInterface.NozzleConditions(5.0, 1.0, "HYD40")
+    return CEAInterface.get_exp_ratio(nozzle_cond) > 1
+end
+
+function test_add_new_propellant()
+    CEAInterface.add_new_propellant("N2",
+    ['N'], [2], 0.0, 298.15, 1.132e-3)
+    ret = false
+    try
+        nozzle_cond = CEAInterface.NozzleConditions(5.0, 1.0, "N2")
+        CEAInterface.get_ambient_Isp(nozzle_cond, 1.5)
+        ret = true
+    catch
+    end
+    return ret
+end
+
+@testset "Test rocket cea" begin
+    @test test_rocket_cea()
+    @test test_get_ambient_Isp()
+    @test test_get_ambient_cf()
+    @test test_get_Cstar()
+    @test test_get_densities()
+    @test test_get_exit_mach()
+    @test test_get_sonic_speeds()
+    @test test_get_temperatures()
+    @test test_get_exp_ratio()
+    @test test_add_new_propellant()
 end
 end
-@test RocketCeaTest.test_rocket_cea() ≈ 374.30361765576265
