9e66e0321de5c7ec5ec5dd84d02140899ba788df
pekpuglia <pepuglia@gmail.com>
2022-08-10 15:14:28 -0300

    thrust analysis

--- nozzle_design/scripts/thrust_analysis.jl
+++ nozzle_design/scripts/thrust_analysis.jl
@@ -1,18 +1,19 @@
 using Plots
 include("../src/nozzle_design.jl")
 ##
-
-function mdot(Pchamber::Float64, F::Float64)
-    ncond = CEAInterface.OperatingCondition(Pchamber, 1.0, "N2", 298.15)
-    return get_nozzle_mdot(F, ncond)
-end
-##
-Pchambers = 1.1:0.1:10.0
+Pchambers = 150.0:50:1000
 Fs = 0.1:0.1:20.0
-mdots = mdot.(Pchambers', Fs)
+mdots = get_nozzle_mdot.(Fs, CEAInterface.OperatingCondition.(Pchambers', 100.0, "N2", 298.15))
 ##
-
 contour(Pchambers, Fs, mdots, levels = 20, fill=true)
-xlabel!("Chamber pressure (atm)")
+xlabel!("Chamber pressure")
 ylabel!("Thrust (N)")
 title!("Mass flow (kg/s)")
+png("./nozzle_design/scripts/thust_analysis.png")
+##
+ncond = CEAInterface.OperatingCondition(500.0, 100.0, "N2", 298.15)
+coeffs = CEACoeffs(ncond)
+mdot = get_nozzle_mdot(5.0, ncond)
+display(ncond)
+dump(coeffs)
+println("m_dot = $mdot")
