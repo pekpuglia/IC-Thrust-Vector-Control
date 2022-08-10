c57a98f4f8f1486a55cc26f8c6e0efeba8eb24d8
pekpuglia <pepuglia@gmail.com>
2022-08-09 22:21:40 -0300

    draft of nozzle design

---
+++ nozzle_design/src/nozzle_design.jl
@@ -0,0 +1,34 @@
+include("cea_interface.jl")
+
+struct CEACoeffs
+    eps::Float64
+    Cstar::Float64
+    Cf::Float64
+end
+
+function CEACoeffs(ncond::CEAInterface.NozzleConditions)
+    eps = CEAInterface.get_exp_ratio(ncond)
+    Cstar = CEAInterface.get_Cstar(ncond)
+    Cf = CEAInterface.get_ambient_Cf(ncond, eps)[2]
+    return CEACoeffs(eps, Cstar, Cf)
+end
+
+function get_nozzle_mdot(F::Float64, ncond::CEAInterface.NozzleConditions)
+    coeffs = CEACoeffs(ncond)
+    return F / (coeffs.Cf * coeffs.Cstar)
+end
+
+struct NozzleAreas
+    Achamber::Float64
+    Athroat::Float64
+    Aexit::Float64
+end
+
+function NozzleAreas(F::Float64, ncond::CEAInterface.NozzleConditions,
+            contraction_ratio::Float64 = 10.0)
+    coeffs = CEACoeffs(ncond)
+    At = F / (ncond.Pc * coeffs.Cf)
+    Ae = At * coeffs.eps
+    Ac = At * contraction_ratio
+    return NozzleAreas(Ac, At, Ae)
+end
