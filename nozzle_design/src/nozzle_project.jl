bc9690e29153a69284244ad0e166e328bd0d7e73
pekpuglia <pepuglia@gmail.com>
2022-08-16 21:21:12 -0300

    added units

--- nozzle_design/src/nozzle_project.jl
+++ nozzle_design/src/nozzle_project.jl
@@ -1,58 +1,59 @@
 module NozzleProject
 using ..CEAInterface
+using ..Unitful
 struct CEACoeffs
     eps::Float64
-    Cstar::Float64
+    Cstar::Quantity
     Cf::Float64
 end
 
 function CEACoeffs(ncond::OperatingCondition)
     eps = CEAInterface.get_exp_ratio(ncond)
-    Cstar = CEAInterface.get_Cstar(ncond)
+    Cstar = CEAInterface.get_Cstar(ncond) * 1u"m/s"
     Cf = CEAInterface.get_ambient_Cf(ncond, eps)[2]
     return CEACoeffs(eps, Cstar, Cf)
 end
 export get_mdot
-function get_mdot(F::Float64, ncond::OperatingCondition)
+function get_mdot(F::Quantity, ncond::OperatingCondition)
     coeffs = CEACoeffs(ncond)
-    return F / (coeffs.Cf * coeffs.Cstar)
+    return uconvert(u"kg/s", F / (coeffs.Cf * coeffs.Cstar))
 end
 
 export NozzleAreas
 struct NozzleAreas
-    Achamber::Float64
-    Athroat::Float64
-    Aexit::Float64
+    Achamber::Quantity
+    Athroat::Quantity
+    Aexit::Quantity
 end
 
-function NozzleAreas(F::Float64, ncond::OperatingCondition;
+function NozzleAreas(F::Quantity, ncond::OperatingCondition;
     contraction_ratio::Float64 = 10.0,
-    min_chamber_radius_mm::Float64 = 0.0)
+    min_chamber_radius::Quantity = 0.0u"mm")
 
     coeffs = CEACoeffs(ncond)
-    At = 1e6*(F / (1e3*ncond.Pc * coeffs.Cf))   #mm²
+    At = uconvert(u"mm^2", (F / (ncond.Pc * coeffs.Cf)))   #mm²
     Ae = At * coeffs.eps    #mm²
-    Ac = max(At * contraction_ratio, π*min_chamber_radius_mm^2) #mm²
+    Ac = max(At * contraction_ratio, π*min_chamber_radius^2) #mm²
     return NozzleAreas(Ac, At, Ae)
 end
 
 export get_radii, get_radius
-get_radius(A::Float64) = √(A/π)
+get_radius(A) = √(A/π)
 get_radii(noz::NozzleAreas)= get_radius(noz.Achamber),
                                 get_radius(noz.Athroat),
                                 get_radius(noz.Aexit)
 export NozzleGeometry
 struct NozzleGeometry
     areas::NozzleAreas
-    exit_half_angle::Float64
-    conv_half_angle::Float64
-    chamber_length::Float64
-    thickness::Float64
+    exit_half_angle::Quantity
+    conv_half_angle::Quantity
+    chamber_length::Quantity
+    thickness::Quantity
 end
 
 get_radii(noz::NozzleGeometry) = get_radii(noz.areas)
-export generate_flow_wall
-function generate_flow_wall(noz::NozzleGeometry)
+export round_flow_wall
+function round_flow_wall(noz::NozzleGeometry)
     rchamber, rthroat, rexit = get_radii(noz)
 
     return cat.([
@@ -64,9 +65,19 @@ function generate_flow_wall(noz::NozzleGeometry)
         accumulate(+, [
             0
             noz.chamber_length
-            (rchamber - rthroat) / tand(noz.conv_half_angle)
-            (rexit - rthroat) / tand(noz.exit_half_angle) 
+            (rchamber - rthroat) / tan(noz.conv_half_angle)
+            (rexit - rthroat) / tan(noz.exit_half_angle) 
         ]), dims=1)
+end
+
+export pipe_speed
+function pipe_speed(mdot::Quantity, opcond::OperatingCondition, pipe_diam::Quantity)
+    uconvert(u"m/s",
+    mdot * 8.314u"J/K/mol" *
+         opcond.propellant.temperature /
+          (π * (pipe_diam/2) ^ 2 * opcond.Pc *
+                         opcond.propellant.molar_mass)
+    )
 end
 
 end
