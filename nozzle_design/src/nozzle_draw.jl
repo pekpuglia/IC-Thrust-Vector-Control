76fd8fa63d5cc0c3dea017cf57b94dc12d9860d2
pekpuglia <pepuglia@gmail.com>
2022-08-24 19:19:32 -0300

    SIde hole

--- nozzle_design/src/nozzle_draw.jl
+++ nozzle_design/src/nozzle_draw.jl
@@ -93,63 +93,81 @@ function (ch::ConnectorHole)(solid, nozgeom::RoundNozzle)
             * linear_extrude(mm(ch.depth)+√eps()) * circle(mm(ch.diam)/2))
     # solid \ (ch.center+linear_extrude(mm(ch.depth)) * circle(mm(ch.diam)/2))
 end
-export HexBase
-struct HexBase <: NozzleFeature
+export PolyBase
+struct PolyBase <: NozzleFeature
     height::Quantity
     clearance::Quantity
+    n::Int
 end
-hexagon(inner_r::Float64) = [(2*inner_r/sqrt(3)) .* reverse(sincospi((i+1/6))) for i in (0:5)/3]
+poly(inner_r::Number, n::Int) = [inner_r / cos(π/n) .* reverse(sincospi((2*i + 1/n))) for i in (0:(n-1))/n]
 
-side_length(hb::HexBase, ng::RoundNozzle) =  2/√3 *
-         (get_radius(ng.areas.Achamber) + ng.thickness + hb.clearance)
+side_length(inner_r::Number, n::Int) = 2 * inner_r * tan(π/n)
+side_length(nozgeom::RoundNozzle, pb::PolyBase) = side_length(
+    get_radius(nozgeom.areas.Achamber)+nozgeom.thickness+pb.clearance, pb.n)
 
-function (hb::HexBase)(solid, nozgeom::RoundNozzle)
+
+function (pb::PolyBase)(solid, nozgeom::RoundNozzle)
     t = mm(nozgeom.thickness)
     out_r = mm(get_radius(nozgeom.areas.Achamber)) + t
     return union(
         solid,
-        [0,0,-t] + linear_extrude(mm(hb.height)) *
-            (polygon(hexagon(out_r+mm(hb.clearance))) \ circle(out_r))
+        [0,0,-t] + linear_extrude(mm(pb.height)) *
+            (polygon(poly(out_r+mm(pb.clearance), pb.n)) \ circle(out_r))
     )
 end
 
-export ProbeHole
-struct ProbeHole <: NozzleFeature
+export Pad
+struct Pad <: NozzleFeature
     width::Quantity
     height::Quantity
     pad_clearance::Quantity
-    diam::Quantity
     z_offset::Quantity
+    x_angle::Quantity
 end
 
-function (ph::ProbeHole)(solid, nozgeom::RoundNozzle)
+function (p::Pad)(solid, nozgeom::RoundNozzle)
+    t = mm(nozgeom.thickness)
+    rcham = mm(get_radius(nozgeom.areas.Achamber))
+    len = rcham + t + mm(p.pad_clearance)
+    pad_base = rotate(ustrip(Float64, u"°", p.x_angle)) *
+            ([len/2, 0] + square(len, mm(p.width), center=true)
+        ) \ circle(rcham+t)
+
+    union(
+        solid,
+        [0,0, mm(p.z_offset)] 
+        + linear_extrude(mm(p.height)) * pad_base
+    )
+end
+
+export SideHole
+struct SideHole <: NozzleFeature
+    width::Quantity
+    height::Quantity
+    pad_clearance::Quantity
+    z_offset::Quantity
+    x_angle::Quantity
+    diam::Quantity
+end
+
+function (ph::SideHole)(solid, nozgeom::RoundNozzle)
     rchamQ = get_radius(nozgeom.areas.Achamber)
     connhole = ConnectorHole(ph.diam, 
         ph.pad_clearance+rchamQ+nozgeom.thickness,
         [0,0,mm(ph.z_offset+ph.height/2)],
-        [1.0,0,0]
+        [cos(ph.x_angle),sin(ph.x_angle),0]
     )
+    pad = Pad(ph.width, ph.height, ph.pad_clearance, ph.z_offset, ph.x_angle)
 
-    t = mm(nozgeom.thickness)
-    rcham = mm(rchamQ)
-    len = rcham + t + mm(ph.pad_clearance)
-    
-    pad_base = ([len/2, 0] + square(len,
-                    mm(ph.width),
-                    center=true)
-                ) \ circle(rcham+t)
-
-    connhole(union(
-        solid,
-        [0,0, mm(ph.z_offset)] 
-        + linear_extrude(mm(ph.height)) * pad_base
-    ), nozgeom)
+    connhole(pad(solid, nozgeom), nozgeom)
 end
+
 export build_nozzle
-function build_nozzle(ng::RoundNozzle, features::Vector{NozzleFeature})
+function build_nozzle(ng::RoundNozzle, features...)
     reduce((base, feat) -> feat(base, ng), features, init = ng())
 end
 
+export export_stl
 function export_stl(file::String, solid; rtol=1e-3, atol=1e-3)
     ConstructiveGeometry.stl(file, solid, rtol=rtol, atol=atol)
 end
