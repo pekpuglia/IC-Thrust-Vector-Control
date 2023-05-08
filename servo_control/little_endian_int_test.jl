a8b5e6a9882283025f2244312631473270c02665
pekpuglia <pepuglia@gmail.com>
2023-05-08 14:08:19 -0300

    script teste

---
+++ servo_control/little_endian_int_test.jl
@@ -0,0 +1,21 @@
+using LibSerialPort
+
+LibSerialPort.open("/dev/ttyACM0", 9600) do sp
+    while true
+        input = readline()
+        if input == "q"
+            return
+        end
+        int_to_send = try
+            parse(Int32, input)
+        catch e
+            if e isa ArgumentError
+                println("input deve ser numérico")
+                continue
+            else
+                throw(e)
+            end
+        end
+        write(sp, htol(int_to_send), '\n')
+    end
+end
