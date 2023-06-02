78ce2a49af4756a8c883c4dda3ab0d54f1bf596e
pekpuglia <pepuglia@gmail.com>
2023-06-01 22:34:50 -0300

    correct calibration?

--- 2dof_test/analysis.jl
+++ 2dof_test/analysis.jl
@@ -1,33 +1,12 @@
 using Plots, Unitful, CSV
 ##
-function bA(datafile, b_column)
+function bA(datafile)
     data = CSV.File(joinpath(@__DIR__, "data/", datafile), header = false)
     n=length(data)
-    b = zeros(n, 3)
-    b[:, b_column] .= data.Column1
-    A = [data.Column2 data.Column4 data.Column6 ones(n,1)]
+    b = data.Column1
+    A = [data.Column2 data.Column4 data.Column6]
     return b, A
 end
-##
-bA_tuples = bA.(
-    [
-    "Cal_empuxo_Crescente.txt"
-    "Cal_empuxo_Decrescente.txt"
-    "Cal_força_Lateral_Crescente.txt"
-    "Cal_força_Lateral_Decrescente.txt"], [1, 1, 2, 2]) 
-
-#[Fy Fx M]
-b = cat(first.(bA_tuples)..., dims=1)
-A = cat(last.(bA_tuples)..., dims=1)
-##
-#[Fy Fx M] = [A F D 1] coefs
-coefs = A \ b
-##
-#calib do momento
-
-##
-scatter(b[:, 2], A * coefs[:,2], label="")
-plot!(identity, b[:, 2], line=:dash, color=:red, label="")
 ##
 struct AFDDataPoint
     label
@@ -38,10 +17,18 @@ end
 
 function load_AFD(filename)
     data = CSV.File(joinpath(@__DIR__,"data/", filename), header=false)
-    ret = (data.Column2, data.Column4, data.Column6)
     AFDDataPoint.(data.Column1, data.Column2, data.Column4, data.Column6)
 end
 
+function calibrate(Fy_series::Vector{AFDDataPoint}, Fx_series::Vector{AFDDataPoint})
+    FA = getproperty.(Fy_series, :label)/2
+    FF = getproperty.(Fy_series, :label)/2
+    FD = getproperty.(Fx_series, :label)
+
+    
+
+end
+##
 struct FxFyM
     label
     Fy
@@ -50,37 +37,52 @@ struct FxFyM
 end
 
 function FxFyM(afd::AFDDataPoint, coefs::Matrix)
-    FxFyM(afd.label, ([afd.A afd.F afd.D 1] * coefs)...)
 end
 
 function FxFyM(afds::Vector, coefs::Matrix)
-    fyfxm = eachrow([getproperty.(afds, [:A :F :D]) ones(length(afds))] * coefs)
-    FxFyM.(getproperty.(afds, :label), getindex.(fyfxm,1), getindex.(fyfxm,2), getindex.(fyfxm,3))
 end
 
 Ft(f::FxFyM) = √(f.Fx^2+f.Fy^2)
+##
+FybA_tuples = bA.(
+    [
+    "Cal_empuxo_Crescente.txt"
+    "Cal_empuxo_Decrescente.txt"]
+)
 
-Base.getproperty(x::Vector{FxFyM}, s::Symbol) = getproperty.(x, s)
+Fy_input = cat(first.(FybA_tuples)..., dims=1)
+Fy_output = cat(last.(FybA_tuples)..., dims=1)
+Fy_seccol = ones(length(Fy_input))
 ##
-#teste com empuxo sem aleta
-raw_thrustAFD = load_AFD("empuxo_sem_aleta.txt")
+FA = Fy_input/2
+Acoefs = [Fy_output[:,1] Fy_seccol] \ FA
 ##
-raw_thrustFxFy = FxFyM(raw_thrustAFD, coefs)
+FF = Fy_input/2
+Fcoefs = [Fy_output[:,2] Fy_seccol] \ FF
 ##
-p = scatter(Ft.(raw_thrustFxFy), label="", xlabel="Medida", ylabel="Força (g)", title="Empuxo sem aleta", dpi=400)
-vline!(p, findall(x -> x > 0, diff(raw_thrustFxFy.label)), line = :dash, label = "")
-png(p, joinpath(@__DIR__, "output/", "empuxo_sem_aleta.png"))
+FxbA_tuples = bA.(
+    [
+        "Cal_força_Lateral_Crescente.txt"
+        "Cal_força_Lateral_Decrescente.txt"
+    ]
+)
+Fx_input = cat(first.(FxbA_tuples)..., dims=1)
+Fx_output = cat(last.(FxbA_tuples)..., dims=1)
+Fx_seccol = ones(length(Fx_input))
 ##
-input_file = "exp8_5bar_110_a_70"
-expAFD = load_AFD(input_file*".txt")
+Dcoefs = [Fx_output[:,3] Fx_seccol] \ Fx_input
 ##
-expFxFyM = FxFyM(expAFD, coefs)
-expFxFyM = FxFyM.(expFxFyM.label, expFxFyM.Fy, expFxFyM.Fx .- expFxFyM[11].Fx, expFxFyM.M)
+scatter(FA, [Fy_output[:,1] Fy_seccol] * Acoefs)
+png("FA_scatter")
 ##
-p = plot(expFxFyM.label, Ft.(expFxFyM), label="Força total", title="Forças em função da deflexão", xlabel="Ângulo do servo", ylabel="Força (g)")
-plot!(p, expFxFyM.label, expFxFyM.Fx, label="Força lateral")
-plot!(p, expFxFyM.label, expFxFyM.Fy, label="Empuxo")
+scatter(FF, [Fy_output[:,2] Fy_seccol] * Fcoefs)
+png("FF_scatter")
 ##
-png(p, joinpath(@__DIR__, "output/", input_file))
+scatter(Fx_input, [Fx_output[:,3] Fx_seccol] * Dcoefs)
+png("FD_scatter")
+## #####################################################3
+scatter(Fy_input, [Fy_output[:,1] Fy_seccol] * Acoefs + [Fy_output[:,2] Fy_seccol] * Fcoefs)
+png("Fy_scatter")
 ##
-plot(expFxFyM.Fx, expFxFyM.Fy)
+scatter([Fy_output[:,1] Fy_seccol] * Acoefs - [Fy_output[:,2] Fy_seccol] * Fcoefs)
+png("M_scatter")
